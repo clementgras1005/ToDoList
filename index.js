5cc0e9699877e23542cabbcd0bab5b020de7e767
clementgras1005 <clementgras1005@gmail.com>
2023-02-23 20:04:28 +0100

    Creation des fichiers nécessaire au projet ToDoList

---
+++ index.js
@@ -0,0 +1,34 @@
+//create a todo
+function createToDo(task){
+
+}
+
+//update the text of the todo
+function updateToDo(newTask){
+
+}
+
+//Todo finished. Background change color
+function finishToDo(){
+
+}
+
+//delete one todo
+function deleteToDo(){
+
+}
+
+//delete all selected todo
+function deleteMultipleTodo(){
+
+}
+
+//displays the todo list
+function renderTodoList(){
+
+}
+
+//save data in local storage
+function saveToLocalStorage(){
+
+}
